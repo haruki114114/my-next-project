321756b6ee851285253786e9cd683f0df396f6a0
haruki114114 <ktc24a31e0011@edu.kyoto-tech.ac.jp>
2024-11-02 15:20:29 +0900

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return(
+    <div>
+      <h1 className="title">テクノロジーの力で世界を変える</h1>
+      <p className="description">私たちは市場をリードしているグローバルテックカンパニーです</p>
+    </div>
+  );
+}
