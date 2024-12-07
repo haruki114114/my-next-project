ff6a0d5114cabc53c54ec14e79638ff48a76de0f
haruki114114 <ktc24a31e0011@edu.kyoto-tech.ac.jp>
2024-12-07 16:25:07 +0900

    184完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -4,6 +4,7 @@ import type {
   MicroCMSImage,
   MicroCMSListContent,
 } from "microcms-js-sdk";
+import NewsList from "../_components/NewsList";
 
 export type Member = {
   name: string;
@@ -14,17 +15,15 @@ export type Member = {
 
 export type Category = {
   name: string;
-}
+} & MicroCMSListContent;
 
 export type News = {
-  id: string;
   title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+  description: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
@@ -47,3 +46,24 @@ export const getMemberList = async (queries?: MicroCMSQueries) => {
     })
   return listData
 }
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client
+    .getList<News>({
+      endpoint: "news",
+      queries,
+    });
+  return listData;
+};
+
+export const getNewsDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  const detailData = await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+  return detailData;
+};
